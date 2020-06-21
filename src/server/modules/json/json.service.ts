dae216d8b0d9119eca6cd1c59d39bcfb83f20b53
Marc Mintel <marc@mintel.me>
2020-06-21 20:39:46 +0700

    remove comment

--- src/server/modules/json/json.service.ts
+++ src/server/modules/json/json.service.ts
@@ -56,25 +56,6 @@ export class JsonService {
     return json;
   }
 
-  // async list(path: string, recursive = false): Promise<Array<string>> {
-  //   this.logger.verbose(`list dir: ${path}`);
-  //   if (recursive) {
-  //     return fg(`${path}/**/*.json`);
-  //   }
-
-  //   return fg(`${path}/*.json`);
-  // }
-
-  // async readDir<T>(path: string, recursive = false): Promise<Array<T>> {
-  //   this.logger.verbose(`read dir: ${path}`);
-  //   const dir = await this.list(path, recursive);
-  //   return Promise.all(
-  //     dir.map(async (fileName: string) => {
-  //       return this.readFile<any>(fileName);
-  //     }),
-  //   );
-  // }
-
   private buildIdentifier(path: string) {
     let jsonPath = path;
     const json = '.json';
